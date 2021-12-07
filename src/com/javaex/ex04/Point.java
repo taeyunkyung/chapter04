cdb35693f209e00ac6d28be127ab0d8b4d0e9a8d
경태윤 <xodbsrud320@gmail.com>
2021-12-07 17:04:23 +0900

    ch04_2_2

--- src/com/javaex/ex04/Point.java
+++ src/com/javaex/ex04/Point.java
@@ -1,5 +1,7 @@
 package com.javaex.ex04;
 
+import java.util.Objects;
+
 public class Point {
 
 	private int x;
@@ -33,6 +35,21 @@ public class Point {
 	@Override
 	public String toString() {
 		return "Point [x=" + x + ", y=" + y + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return x + y; // 더 복잡하게 만들면 1단계에서 거를 수 있음, 이클립스에서 정의한 것
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Point p = (Point) obj;
+		if ((this.x == p.x) && (this.y == p.y)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
